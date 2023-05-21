eee027264b447fee48523e360810621de7d4ebb4
Haseeb848 <haseeb@amperortech.com>
2023-05-21 16:16:13 +0500

    cors-enable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  // Serve static files from the "uploads" directory
+  // app.use('/uploads', express.static('uploads'));
   await app.listen(3000);
 }
 bootstrap();
