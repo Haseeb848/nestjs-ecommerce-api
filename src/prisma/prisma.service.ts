5e59bf6ad606fc43d2b52646b7d8a3b8a651d9c4
Haseeb848 <haseebamperortech.com>
2023-05-22 00:31:25 +0500

    db url updated

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,10 +10,13 @@ export class PrismaService extends PrismaClient {
         db: {
           url: config.get(
             'DATABASE_URL',
-            'postgresql://postgres:admin@localhost:5432/shopping',
+            'postgresql://ud8nm5jgzhtfuznwujvn:1ERsSoXwUJNaE8eV49eG@bapiiva7nl372ypest92-postgresql.services.clever-cloud.com:6580/bapiiva7nl372ypest92',
           ),
         },
       },
     });
   }
 }
+
+
+// postgresql://postgres:admin@localhost:5432/shopping
