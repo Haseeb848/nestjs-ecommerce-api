df98c42f998531a74e8f243c67179c83783707a1
Haseeb848 <haseeb@amperortech.com>
2023-05-21 16:15:36 +0500

    cloudinary-modify

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,25 +4,41 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Customer, Product } from '@prisma/client';
 
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+
 @Injectable()
 export class ProductService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
 
   //   async create(createProductDto: CreateProductDto) {
   //     return this.prisma.product.create({ data: createProductDto });
   //   }
 
-  async createProduct(createProductDto: CreateProductDto, user: any, file: Express.Multer.File) {
+  async createProduct(
+    createProductDto: CreateProductDto,
+    user: any,
+    file: Express.Multer.File,
+  ) {
     const { customerId, role } = user;
-    const {filename} = file;
+    // const {filename} = file;
 
-      console.log('User ID:', customerId);
-      console.log('User role:', role);
+    console.log('User ID:', customerId);
+    console.log('User role:', role);
+
+    let imageLink = null;
+
+     if (file) {
+       const result = await this.cloudinaryService.uploadImage(file); // use CloudinaryService to upload image
+       imageLink = result.secure_url;
+     }
 
     const newProduct = {
       name: createProductDto.name,
       price: Number(createProductDto.price),
-      image: filename,
+      image: imageLink,
       createdByCustomerId: customerId,
       createdByRole: role,
     };
@@ -48,6 +64,19 @@ export class ProductService {
 
   findAll() {
     return this.prisma.product.findMany();
+    // return this.prisma.product.findMany().then((products) => {
+    //   // Map over the products and add the image URL for each product
+    //   const productsWithImageURL = products.map((product) => {
+    //     const image = `/api/images/${product.image}`; // Assuming the image filename is stored in the "imageFilename" field of the product
+
+    //     return {
+    //       ...product,
+    //       image,
+    //     };
+    //   });
+
+    //   return productsWithImageURL;
+    // });
   }
 
   findOne(id: number) {
