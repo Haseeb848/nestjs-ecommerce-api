0211154be9d1972f3a869132483bd3f06207a8d3
Haseeb848 <haseeb@amperortech.com>
2023-05-21 16:18:27 +0500

    intend-cloudinary

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaService } from 'src/prisma.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Module({
-  providers: [ProductService],
+    imports: [],
+  providers: [ProductService,CloudinaryService],
   controllers: [ProductController],
 })
 export class ProductModule {}
